c3f47e15e2a06a893536a37e6f9c40cc6d874011
mlatinov <latinovmetodi@gmail.com>
2026-05-10 00:49:43 +0300

    add vector operations to simplefy equations and algorithms implementaions

---
+++ src/Statistics/Vector/Operations.hs
@@ -0,0 +1,18 @@
+{- HLINT ignore "Use camelCase" -}
+module Statistics.Vector.Operations(vs_add, vs_sub, v_add, v_sub) where
+
+--Vector x Scalar addition 
+vs_add :: [Double] -> Double -> [Double]
+vs_add x y = [a + y | a <- x] 
+
+--Vector x Scalar Substraction
+vs_sub :: [Double] -> Double -> [Double]
+vs_sub x y = [a - y | a <- x] 
+
+-- Vector addition 
+v_add :: [Double] -> [Double] -> [Double]
+v_add x y = zipWith (+) x y
+
+-- Vector Substract
+v_sub :: [Double] -> [Double] -> [Double]
+v_sub x y = zipWith (-) x y
